529ee358516654f51afea2c500796dcc7a3d6260
ghenosec <lucasgheno600@gmail.com>
2024-12-23 15:58:18 -0300

    refactor: change const and remove comments

--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -3,7 +3,6 @@
 import React, { useState } from "react";
 import { JSX } from "react/jsx-runtime";
 
-// Importar os componentes das seções
 import Home from '../pages/Home'
 import About from '../pages/About';
 import Experience from '../pages/Experience';
@@ -11,7 +10,7 @@ import Projects from '../pages/Projects';
 
 const Navigation = ({ setActiveSection }: { setActiveSection: (section: string) => void }) => (
   <nav className="w-1/7 p-4 text-left border-r border-gray-300">
-    <h1 className="font-bold text-2xl text-gray-800 mb-4">Lucas Gheno</h1>
+    <h1 className="font-bold text-2xl text-gray-800 mb-4 mt-4">Lucas Gheno</h1>
     <h2 className="text-gray-700 mb-8">Portfólio Website</h2>
     <ul className="space-y-7">
       <li className="mt-64">
@@ -50,10 +49,8 @@ const Navigation = ({ setActiveSection }: { setActiveSection: (section: string) 
   </nav>
 );
 
-const OldSchoolLinuxStyle: React.FC = () => {
+const Sidebar: React.FC = () => {
   const [activeSection, setActiveSection] = useState<string>("home");
-
-  // Função para renderizar a seção com base no estado
   const renderSection = () => {
     switch (activeSection) {
       case "home":
@@ -77,4 +74,4 @@ const OldSchoolLinuxStyle: React.FC = () => {
   );
 };
 
-export default OldSchoolLinuxStyle;
+export default Sidebar;
