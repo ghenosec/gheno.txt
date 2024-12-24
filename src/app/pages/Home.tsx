2de67601835a3e502d99b7479c249b09cee822cc
ghenosec <lucasgheno600@gmail.com>
2024-12-24 11:51:15 -0300

    refactor: corrected words and changed name

--- src/app/pages/Home.tsx
+++ src/app/pages/Home.tsx
@@ -10,12 +10,12 @@ const ResumePage: React.FC = () => {
             height={355}
             className="mx-auto w-56"
           />
-        <h1 className="text-5xl font-bold mt-4">GH-OS</h1>
+        <h1 className="text-5xl font-bold mt-4">GH.txt</h1>
       </div>
       <h1 className="text-4xl font-bold mb-4">Propósito</h1>
       <p>
-       Precisava de um site para seu meu currículo e portfólio, depois de observar vários websites e portfólios, encontrei um seguindo o estilo de sistemas operacionais antigos e gostei demais da ideia,
-        então decidi criar o meu próprio. Ainda não está 100% como eu gostaria, mas estou trabalhando nisso.
+       Precisava de um site para ser meu currículo e portfólio, depois de observar vários websites e portfólios, encontrei um seguindo o estilo de sistemas operacionais antigos e gostei demais da ideia,
+        então decidi recriar com uma ideia que eu tinha em mente. Ainda não está 100% como eu gostaria, mas estou trabalhando nisso.
       </p>
       <h1 className="text-4xl font-bold mt-8 mb-4">Aviso</h1>
       <pre className="bg-gray-200 p-4">
@@ -28,8 +28,8 @@ A ideia é testar novos conceitos, ideias e aprender, então não garanto um có
 
       <h1 className="text-4xl font-bold mt-8 mb-4">Descrição</h1>
       <ul className="list-disc pl-8">
-        <li>Esse projeto imita o start de um Sistema Operacional, aonde após "ligar" mostra algumas informações que englobam a minha pessoa.</li>
-        <li>A parte principal OS do projeto é feito com <a href="https://svelte.dev" className="text-retroGreen">Svelte</a> mas, convertido para <a href="https://www.typescriptlang.org/" className="text-retroGreen">TypeScript</a>, pois é aonde tenho mais familiaridade.</li>
+        <li>Esse projeto imita o start de um Sistema Operacional, aonde após "ligar", automaticamente abre um .txt que mostra algumas informações que englobam a minha pessoa.</li>
+        <li>A parte inicial do projeto é com base em <a href="https://svelte.dev" className="text-retroGreen">Svelte</a> mas, convertido para <a href="https://www.typescriptlang.org/" className="text-retroGreen">TypeScript</a>, pois é aonde tenho mais familiaridade.</li>
         <li>A base do projeto é feita com <a href="https://nextjs.org/" className="text-retroGreen">Next</a> o que me permite testar vários frameworks além de ter suporte nativo para TypeScript e futuras integrações simlpificadas com APIs.</li>
       </ul>
 
