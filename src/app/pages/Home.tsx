49521118c040abfe6614aa4aa23ba0d101aa4731
ghenosec <lucasgheno600@gmail.com>
2024-12-24 12:38:24 -0300

    feat: added transition

--- src/app/pages/Home.tsx
+++ src/app/pages/Home.tsx
@@ -1,26 +1,39 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 
 const ResumePage: React.FC = () => {
+  const [isFadingIn, setIsFadingIn] = useState(false);
+
+  useEffect(() => {
+    setIsFadingIn(true);
+  }, []);
+
   return (
-    <div className="p-4">
+    <div
+      className={`p-4 transition-opacity duration-1000 ${
+        isFadingIn ? "opacity-100" : "opacity-0"
+      }`}
+    >
       <div className="text-center mb-8">
-          <img
-            src="/pc-art.png"
-            alt="Logo"
-            height={355}
-            className="mx-auto w-56"
-          />
+        <img
+          src="/pc-art.png"
+          alt="Logo"
+          height={355}
+          className="mx-auto w-56"
+        />
         <h1 className="text-5xl font-bold mt-4">GH.txt</h1>
       </div>
       <h1 className="text-4xl font-bold mb-4">Propósito</h1>
       <p>
-       Precisava de um site para ser meu currículo e portfólio, depois de observar vários websites e portfólios, encontrei um seguindo o estilo de sistemas operacionais antigos e gostei demais da ideia,
-        então decidi recriar com uma ideia que eu tinha em mente. Ainda não está 100% como eu gostaria, mas estou trabalhando nisso.
+        Precisava de um site para ser meu currículo e portfólio, depois de
+        observar vários websites e portfólios, encontrei um seguindo o estilo
+        de sistemas operacionais antigos e gostei demais da ideia, então decidi
+        recriar com uma ideia que eu tinha em mente. Ainda não está 100% como
+        eu gostaria, mas estou trabalhando nisso.
       </p>
       <h1 className="text-4xl font-bold mt-8 mb-4">Aviso</h1>
       <pre className="bg-gray-200 p-4">
         <code>
-        {`Esse projeto está sendo desenvolvido para aprendizado.
+          {`Esse projeto está sendo desenvolvido para aprendizado.
 É possível encontrar erros, caso encontre, por favor, me avise.
 A ideia é testar novos conceitos, ideias e aprender, então não garanto um código perfeito.`}
         </code>
@@ -28,14 +41,45 @@ A ideia é testar novos conceitos, ideias e aprender, então não garanto um có
 
       <h1 className="text-4xl font-bold mt-8 mb-4">Descrição</h1>
       <ul className="list-disc pl-8">
-        <li>Esse projeto imita o start de um Sistema Operacional, aonde após "ligar", automaticamente abre um .txt que mostra algumas informações que englobam a minha pessoa.</li>
-        <li>A parte inicial do projeto é com base em <a href="https://svelte.dev" className="text-green-500 hover:text-green-700">Svelte</a> mas, convertido para <a href="https://www.typescriptlang.org/" className="text-green-500 hover:text-green-700 ">TypeScript</a>, pois é aonde tenho mais familiaridade.</li>
-        <li>A base do projeto é feita com <a href="https://nextjs.org/" className="text-green-500 hover:text-green-700">Next</a> o que me permite testar vários frameworks além de ter suporte nativo para TypeScript e futuras integrações simlpificadas com APIs.</li>
+        <li>
+          Esse projeto imita o start de um Sistema Operacional, aonde após
+          "ligar", automaticamente abre um .txt que mostra algumas informações
+          que englobam a minha pessoa.
+        </li>
+        <li>
+          A parte inicial do projeto é com base em{" "}
+          <a
+            href="https://svelte.dev"
+            className="text-green-500 hover:text-green-700"
+          >
+            Svelte
+          </a>{" "}
+          mas, convertido para{" "}
+          <a
+            href="https://www.typescriptlang.org/"
+            className="text-green-500 hover:text-green-700 "
+          >
+            TypeScript
+          </a>
+          , pois é aonde tenho mais familiaridade.
+        </li>
+        <li>
+          A base do projeto é feita com{" "}
+          <a
+            href="https://nextjs.org/"
+            className="text-green-500 hover:text-green-700"
+          >
+            Next
+          </a>{" "}
+          o que me permite testar vários frameworks além de ter suporte nativo
+          para TypeScript e futuras integrações simplificadas com APIs.
+        </li>
       </ul>
 
       <h1 className="text-4xl font-bold mt-8 mb-4">Licença</h1>
       <p>
-        Este projeto está sob licença do MIT, fique à vontade para usá-lo como quiser.
+        Este projeto está sob licença do MIT, fique à vontade para usá-lo como
+        quiser.
       </p>
     </div>
   );
--- src/app/pages/Start.tsx
+++ src/app/pages/Start.tsx
@@ -1,32 +1,39 @@
-
 "use client";
 
 import { useState } from "react";
-import { useRouter } from "next/navigation"; 
+import { useRouter } from "next/navigation";
 import BiosLoader from "../components/BiosLoader";
 
 const Start = () => {
   const [showStartScreen, setShowStartScreen] = useState(false);
-  const router = useRouter(); 
+  const [isFadingOut, setIsFadingOut] = useState(false); 
+  const router = useRouter();
 
   const handleBiosCompletion = () => {
     setShowStartScreen(true);
   };
 
   const handleStartClick = () => {
-    router.push("/resume"); 
+    setIsFadingOut(true); 
+    setTimeout(() => {
+      router.push("/resume"); 
+    }, 1000); 
   };
 
   if (showStartScreen) {
     return (
-      <div className="flex flex-col items-center justify-center h-screen bg-black text-white">
+      <div
+        className={`flex flex-col items-center justify-center h-screen bg-black text-white transition-opacity duration-1000 ${
+          isFadingOut ? "opacity-0" : "opacity-100"
+        }`}
+      >
         <div className="border-4 border-white p-6 md:p-8 text-center w-11/12 max-w-md">
           <p className="mb-4 text-lg md:text-xl">Lucas Carmona Gheno 💻 Portfólio</p>
           <p className="mb-6 text-sm md:text-base">
             Click start to begin<span className="blinking-cursor">_</span>
           </p>
           <button
-            onClick={handleStartClick} 
+            onClick={handleStartClick}
             className="border-4 border-white py-2 px-4 hover:bg-white hover:text-black transition"
           >
             START
